d8a2e3b7508cd8ea7a2b650f9995251de9f35459
Smart Huang <Smart.Huang@activenetwork.com>
2015-11-24 12:38:25 +0800

    route for push_to_review

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   post 'orders/oo_unique', to: "orders#is_origin_order_number_valid?"
   post 'orders/op_unique', to: "orders#is_origin_payment_number_valid?"
+  post 'orders/push_to_customs', to: "orders#push_to_customs"
+  post 'orders/push_to_review', to: "orders#push_to_review"
+  post 'orders/decline_order', to: "orders#decline_order"
   resources :orders
   resources :products
   root to: 'visitors#index'
