6f37c855165c9674df51d16cbcef14376e9a2320
Smart Huang <Smart.Huang@activenetwork.com>
2015-11-24 12:38:05 +0800

    push to admin for review

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,11 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.all
+    if current_user.admin?
+      @orders = Order.all
+    else
+      @orders = Order.where(:user_id => current_user.id).all
+    end
   end
 
   # GET /orders/1
@@ -27,6 +31,9 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @order.order_status_id = OrderStatus.find_by_status("未提交").id
+    @order.user_id = current_user.id
+
     respond_to do |format|
       if @order.save
         format.html { redirect_to orders_url, notice: 'Order was successfully created.' }
@@ -41,6 +48,7 @@ class OrdersController < ApplicationController
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
   def update
+    @order.user_id = current_user.id
     respond_to do |format|
       if @order.update(order_params)
         format.html { redirect_to orders_url, notice: 'Order was successfully updated.' }
@@ -55,18 +63,47 @@ class OrdersController < ApplicationController
   # DELETE /orders/1
   # DELETE /orders/1.json
   def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
+    if (@order.user == current_user) or current_user.admin?
+      @order.destroy
+      respond_to do |format|
+        format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
+  end
+
+  def push_to_review
+    # send orders to admin to review
+    respond_to do |format|
+      begin
+        orders = Order.find(params[:orders])
+        orders.each do |o|
+          if current_user == o.user and o.order_status.status == "未提交"
+            o.order_status= OrderStatus.find_by_status("已提交平台")
+            o.save
+          end
+        end
+        format.json { render json: orders, status: :ok}
+      rescue Exception => e
+        format.json { render plain: e}
+      end
+    end
+  end
+
+  def push_to_customs
+    # send orders to customs upstream system
+    # call backend workers
+  end
+
+  def decline_order
+    # order is declined, user need to do update
   end
 
   def is_origin_order_number_valid?
     respond_to do |format|
-      unless Order.find_by_origin_order_number(params[:oo_id])
+      if Order.where(:origin_order_number => params[:oo_id]).count == 0
         format.json { render plain: "true"}
-      else        
+      else
         format.json { render plain: "false"}
       end
     end
@@ -74,9 +111,9 @@ class OrdersController < ApplicationController
 
   def is_origin_payment_number_valid?
     respond_to do |format|
-      unless Order.find_by_origin_payment_number(params[:op_id])
+      if Order.where(:origin_payment_number => params[:op_id]).count == 0
         format.json { render plain: "true"}
-      else        
+      else
         format.json { render plain: "false"}
       end
     end
@@ -91,7 +128,8 @@ class OrdersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def order_params
-    params.require(:order).permit(:order_status_id, :origin_order_number, :origin_payment_number, :customer_name, :phone, :id_number, :province, :city, :area, :street, :zip_code,
+    params.require(:order).permit(:order_status_id, :origin_order_number, :origin_payment_number, :customer_name,
+                                  :phone, :id_number, :province, :city, :area, :street, :zip_code, :shipping_fee,
                                   order_products_attributes: [:product_id,:number, :custom_price, :_destroy, :id])
   end
 
